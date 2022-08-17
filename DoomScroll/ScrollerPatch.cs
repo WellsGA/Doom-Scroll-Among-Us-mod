66547c91da2fd918514c492dfbd42765142054d0
AlainaAK <97328364+AlainaAK@users.noreply.github.com>
2022-08-17 11:24:38 -0700

    Chat scroller chat limit removed
    ObjectPoolBehaviorPatch and ScrollerPatch added for this functionality

---
+++ DoomScroll/ScrollerPatch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HarmonyLib;
+using UnityEngine;
+
+namespace DoomScroll
+{
+    //Disable chat scrolling limit:
+    [HarmonyPatch(typeof(Scroller), nameof(Scroller.Update))]
+    public static class ScrollerUpdatePatch
+    {
+        private static bool m_ScrollerPatchHasRun = false;
+        public static void Postfix(Scroller __instance)
+        {
+
+            if (!m_ScrollerPatchHasRun)
+            {
+                __instance.ContentYBounds = new FloatRange(float.MinValue, float.MaxValue);
+            }
+        }
+    }
+}
