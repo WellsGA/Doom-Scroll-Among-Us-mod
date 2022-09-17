17275ada6add7a464572fe765e032caf6cce1246
AlainaAK <97328364+AlainaAK@users.noreply.github.com>
2022-09-17 14:53:08 -0700

    SecondaryWinCondition structure code
    Should be assigning, tracking, and checking SWCs. Only functionality left to implement (if this runs correctly) would be printing SWCs and results to the player's screen.

---
+++ DoomScroll/MeetingHudRpcVotingCompletePatch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Reactor;
+using HarmonyLib;
+using UnityEngine;
+using DoomScroll.Common;
+
+namespace DoomScroll
+{
+    [HarmonyPatch(typeof(MeetingHud), nameof(MeetingHud.RpcVotingComplete))]
+    public static class MeetingHudRpcVotingCompletePatch
+    {
+        public static void Prefix(MeetingHud __instance, ref GameData.PlayerInfo exiled)
+        {
+            if (exiled != null)
+            {
+                SecondaryWinConditionHolder.checkTargetVotedOut(exiled);
+            }
+        }
+    }
+}
