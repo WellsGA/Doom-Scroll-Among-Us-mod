13a1886b8ffb734b14320cdbea6283fc2ef62012
AlainaAK <97328364+AlainaAK@users.noreply.github.com>
2022-10-04 14:07:09 -0700

    .Info added instead of .Message

--- DoomScroll/RoleManagerSelectRolesPatch.cs
+++ DoomScroll/RoleManagerSelectRolesPatch.cs
@@ -12,6 +12,7 @@ namespace DoomScroll
     {
         public static void Postfix(RoleManager __instance)
         {
+            Logger<DoomScrollPlugin>.Info("Select Roles Patch is running!!\n There should be Secondary Win Conditions below:\n");
             SecondaryWinConditionHolder.clearPlayerSWCList(); // ensures list is empty before filling it
 
             foreach (GameData.PlayerInfo player in GameData.Instance.AllPlayers)
@@ -21,13 +22,13 @@ namespace DoomScroll
                     PlayerSWCTracker secWinCondImpostor = new PlayerSWCTracker(player.PlayerId);
                     secWinCondImpostor.impostorSWC();
                     SecondaryWinConditionHolder.addToPlayerSWCList(secWinCondImpostor);
-                    Logger<DoomScrollPlugin>.Message("" + secWinCondImpostor.getPlayerID() + ", SWC: " + secWinCondImpostor.getSWC());
+                    Logger<DoomScrollPlugin>.Info("" + secWinCondImpostor.getPlayerID() + ", SWC: " + secWinCondImpostor.getSWC());
                 }
                 else if (!player.Role.IsImpostor)
                 {
                     PlayerSWCTracker secWinCondCrewmate = new PlayerSWCTracker(player.PlayerId);
                     SecondaryWinConditionHolder.addToPlayerSWCList(secWinCondCrewmate);
-                    Logger<DoomScrollPlugin>.Message("PID: " + secWinCondCrewmate.getPlayerID() + ", SWC: " + secWinCondCrewmate.getSWC());
+                    Logger<DoomScrollPlugin>.Info("PID: " + secWinCondCrewmate.getPlayerID() + ", SWC: " + secWinCondCrewmate.getSWC());
                 }
             }
 
