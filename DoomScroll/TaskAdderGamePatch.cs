fdf713f0fb24ceb12360ded271ed4cd71ea0ca6b
AlainaAK <97328364+AlainaAK@users.noreply.github.com>
2022-10-18 13:04:30 -0700

    Attempted to add SWC below task list

---
+++ DoomScroll/TaskAdderGamePatch.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Reactor;
+using HarmonyLib;
+using UnityEngine;
+using DoomScroll.Common;
+
+namespace DoomScroll
+{
+    [HarmonyPatch(typeof(TaskAdderGame))]
+    class TaskAdderGamePatch
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch("ShowFolder")]
+        public static void PostfixShowFolder(TaskAdderGame __instance, TaskAddButton taskAddButton)
+        {
+            taskAddButton.Text.text = "SWC: " + SecondaryWinConditionHolder.getSomePlayerSWC(PlayerControl.LocalPlayer._cachedData.PlayerId).SWCAssignText();
+        }
+    }
+}
