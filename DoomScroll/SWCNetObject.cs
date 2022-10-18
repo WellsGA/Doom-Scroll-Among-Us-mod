e707c1b5262db6632317429a63ac05bd7154f441
AlainaAK <97328364+AlainaAK@users.noreply.github.com>
2022-10-18 13:16:06 -0700

    New class for potential RPC object (like GameData)

---
+++ DoomScroll/SWCNetObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Beebyte.Obfuscator;
+using Hazel;
+using InnerNet;
+
+namespace DoomScroll
+{
+    class SWCNetObject: InnerNetObject
+    {
+    }
+}
